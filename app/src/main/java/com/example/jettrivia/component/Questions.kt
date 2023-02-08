5cf1ff0b709215ac772aae621de681f898714edb
matheusgomes2020 <matheusgomez2013@gmail.com>
2023-02-08 17:24:00 -0300

    Creating UI - Radio Button Row.

--- app/src/main/java/com/example/jettrivia/component/Questions.kt
+++ app/src/main/java/com/example/jettrivia/component/Questions.kt
@@ -2,14 +2,22 @@ package com.example.jettrivia.component
 
 import android.util.Log
 import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.PathEffect
 import androidx.compose.ui.text.ParagraphStyle
 import androidx.compose.ui.text.SpanStyle
@@ -20,6 +28,8 @@ import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.example.jettrivia.model.Question
+import com.example.jettrivia.model.QuestionItem
 import com.example.jettrivia.screens.QuestionsViewModel
 import com.example.jettrivia.util.AppColors
 
@@ -47,9 +57,22 @@ fun Questions( viewModel: QuestionsViewModel){
 
     }
 
-@Preview
+//@Preview
 @Composable
-fun QuestionDisplay() {
+fun QuestionDisplay(
+
+    question: QuestionItem,
+    questionIndex: MutableState<Int>,
+    viewModel: QuestionsViewModel,
+    onNextClicked: (Int) -> Unit
+
+        ) {
+
+    val choiceState = remember( question ) {
+
+        question.choices.toMutableList()
+
+    }
 
     val pathEffect = PathEffect.dashPathEffect( floatArrayOf( 10f, 10f ), 0f )
 
@@ -66,6 +89,39 @@ fun QuestionDisplay() {
             QuestionTracker()
 
             DrawDottedLine(pathEffect = pathEffect )
+
+            Column {
+
+                Text(text = "What's the meaning of all this?",
+                    modifier = Modifier
+                        .padding(6.dp)
+                        .align(alignment = Alignment.Start)
+                        .fillMaxHeight(0.3f),
+                    fontSize = 17.sp,
+                    color = AppColors.mOffWhite,
+                    fontWeight = FontWeight.Bold,
+                    lineHeight = 22.sp )
+
+                //choices
+                choiceState.forEachIndexed{ index, answerText ->
+                    Row(modifier = Modifier
+                        .padding(3.dp)
+                        .fillMaxWidth()
+                        .height( 45.dp )
+                        .border( width = 4.dp, brush = Brush.linearGradient(
+                            colors = listOf( AppColors.mDarkPurple,
+                                AppColors.mDarkPurple )),
+                            shape = RoundedCornerShape( 15.dp ) )
+                        .clip( RoundedCornerShape( topStartPercent = 50,
+                                                    topEndPercent = 50,
+                                                    bottomEndPercent = 50,
+                                                    bottomStartPercent = 50) )
+                        .background( Color.Transparent )) {
+
+                    }
+                }
+
+            }
 
         }
 
